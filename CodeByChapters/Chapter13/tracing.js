200cdc24020eac61259aa5a5f0e7b03ada8377ae
Guille Ojeda <guillermo.ojeda@caylent.com>
2026-04-30 07:19:11 -0300

    Fix Chapter 13 OpenTelemetry sample

--- CodeByChapters/Chapter13/tracing.js
+++ CodeByChapters/Chapter13/tracing.js
@@ -1,12 +1,18 @@
 const { NodeSDK } = require('@opentelemetry/sdk-node');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { AWSXRayIdGenerator } = require('@opentelemetry/id-generator-aws-xray');
+const { AWSXRayPropagator } = require('@opentelemetry/propagator-aws-xray');
+
+const traceExporter = new OTLPTraceExporter({
+    url: process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || 'http://localhost:4317',
+});
 
 const sdk = new NodeSDK({
     serviceName: process.env.OTEL_SERVICE_NAME || 'SimpleAWSNodejsApp',
-    traceExporter: new OTLPTraceExporter({
-        url: process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT || 'http://localhost:4318/v1/traces',
-    }),
+    traceExporter,
+    idGenerator: new AWSXRayIdGenerator(),
+    textMapPropagator: new AWSXRayPropagator(),
     instrumentations: [getNodeAutoInstrumentations()],
 });
 
